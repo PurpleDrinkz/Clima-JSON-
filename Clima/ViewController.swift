b034edca308a31a1b24a2717d410088e86fd7b48
Alumno <ulsa@iMacD108.local>
2018-10-23 18:45:50 -0700

    Calculadora de dolar a peso mexicano

--- Clima/ViewController.swift
+++ Clima/ViewController.swift
@@ -11,27 +11,83 @@ import Alamofire
 
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var lblGrados: UILabel!
+    let URLClima = "https://free.currencyconverterapi.com/api/v6/convert?q=USD_MXN&compact=y"
     
+    
+    @IBOutlet weak var lblGrados: UILabel!
+    @IBOutlet weak var aiCargarClima: UIActivityIndicatorView!
+    
+    @IBOutlet weak var doTapActualizarClima: UIButton!
+    @IBAction func doTapActualizarClima(_ sender: Any) {
+        
+        lblGrados.text = "-"
+        aiCargarClima.startAnimating()
+        
+        Alamofire.request(URLClima).responseJSON { response in
+            //Aquì te regresa todo los datos crudos como tal (Codigo JSON)
+            if let dictRespuesta = response.result.value as? NSDictionary{
+                
+                if let dictMain = dictRespuesta.value(forKey: "USD_MXN") as? NSDictionary{
+                    
+                    if let val = dictMain.value(forKey: "val") as? Double {
+                        
+                        self.lblGrados.text = "\(val) peso"
+                        self.aiCargarClima.stopAnimating()
+                    }
+                    
+                }
+                
+            }else{
+                
+                //Avisarle al usuario que no se pudo leer la respuesta
+            }
+            
+            
+        }
+        
+        
+        
+    }
     
     
     
       
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        let URLClima = "http://api.openweathermap.org/data/2.5/weather?q=ciudad+obregon&APPID=3326d4de85a204e001116973cc1e557a&units=metric"
+        
+        
+        super.viewDidLoad()
+        
+        aiCargarClima.startAnimating()
+        
+        
+        
         
         Alamofire.request(URLClima).responseJSON { response in
+            //Aquì te regresa todo los datos crudos como tal (Codigo JSON)
+            if let dictRespuesta = response.result.value as? NSDictionary{
+                
+                if let dictMain = dictRespuesta.value(forKey: "USD_MXN") as? NSDictionary{
+                    
+                    if let val = dictMain.value(forKey: "val") as? Double {
+                        
+                        self.lblGrados.text = "\(val) Peso"
+                        self.aiCargarClima.stopAnimating()
+                    }
+                    
+                }
+                
+            }else{
+                
+                //Avisarle al usuario que no se pudo leer la respuesta
+            }
+            
             
         }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
